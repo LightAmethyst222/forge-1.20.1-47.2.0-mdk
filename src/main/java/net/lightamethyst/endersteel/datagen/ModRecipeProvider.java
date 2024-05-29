f16e30ff1772404e8221db40aca34be981c49b1a
LightAmethyst222 <2wqlucas@gmail.com>
2024-05-29 10:10:07 -0400

    Datageneration Errors not stable

--- src/main/java/net/lightamethyst/endersteel/datagen/ModRecipeProvider.java
+++ src/main/java/net/lightamethyst/endersteel/datagen/ModRecipeProvider.java
@@ -1,4 +1,67 @@
 package net.lightamethyst.endersteel.datagen;
 
-public class ModRecipeProvider {
+import net.lightamethyst.endersteel.Endersteel;
+import net.lightamethyst.endersteel.block.ModBlocks;
+import net.lightamethyst.endersteel.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    public static final List<ItemLike> ENDERSTEEL_SMELTABLES = List.of(ModItems.RAW_ENDERSCRAP.get());
+
+    public ModRecipeProvider(PackOutput pOutput) {
+        super(pOutput);
+    }
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
+        oreBlasting(consumer, ENDERSTEEL_SMELTABLES, RecipeCategory.MISC, ModItems.REFINED_ENDERSCRAP.get(), 2.0f, 200, "endersteel");
+
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.ENDERSTEEL_BLOCK.get())
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .define('#', ModItems.ENDERSTEEL_INGOT.get())
+                .unlockedBy(getHasName(ModItems.ENDERSTEEL_INGOT.get()), has(ModItems.ENDERSTEEL_INGOT.get()))
+                .save(consumer);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.ENDERSTEEL_INGOT.get(), 9)
+                .requires(ModBlocks.ENDERSTEEL_BLOCK.get())
+                .unlockedBy(getHasName(ModBlocks.ENDERSTEEL_BLOCK.get()), has(ModBlocks.ENDERSTEEL_BLOCK.get()))
+                .save(consumer);
+    }
+
+
+
+    protected static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTIme, String pGroup) {
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTIme, pGroup, "_from_smelting");
+    }
+
+    protected static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
+    }
+
+    protected static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+        Iterator var9 = pIngredients.iterator();
+
+        while(var9.hasNext()) {
+            ItemLike itemlike = (ItemLike)var9.next();
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(new ItemLike[]{itemlike}), pCategory, pResult,
+                    pExperience, pCookingTime, pCookingSerializer)
+                    .group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(pFinishedRecipeConsumer, Endersteel.MOD_ID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
+        }
+
+    }
+
 }
--- src/main/java/net/lightamethyst/endersteel/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/lightamethyst/endersteel/datagen/loot/ModBlockLootTables.java
@@ -1,4 +1,40 @@
 package net.lightamethyst.endersteel.datagen.loot;
 
-public class ModBlockLootTables {
+import net.lightamethyst.endersteel.block.ModBlocks;
+import net.lightamethyst.endersteel.item.ModItems;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Set;
+
+public class ModBlockLootTables extends BlockLootSubProvider {
+    public ModBlockLootTables(Set<Item> pExplosionResistant, FeatureFlagSet pEnabledFeatures) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+        this.dropSelf(ModBlocks.STEEL_BLOCK.get());
+        this.dropSelf(ModBlocks.ENDERSTEEL_BLOCK.get());
+
+        this.add(ModBlocks.ENDERSCRAP_ORE.get(),
+                block -> createEnderscrapOreDrop(ModBlocks.ENDERSCRAP_ORE.get(), ModItems.RAW_ENDERSCRAP.get()));
+    }
+    protected LootTable.Builder createEnderscrapOreDrop(Block pBlock, Item pItem) {
+        return createSilkTouchDispatchTable(pBlock, (LootPoolEntryContainer.Builder) this.applyExplosionDecay(pBlock, LootItem.lootTableItem(pItem)));
+    }
+
+        @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
 }
